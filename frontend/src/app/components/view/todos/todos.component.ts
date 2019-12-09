6d42ad23ce193995f33fd75530fb8a9c71c0d2dc
Markos Stamatakis <markosg.stamatakis@gmail.com>
2019-12-09 03:16:34 +0200

    Clean up component
    Clean up todo component by adding http services

--- frontend/src/app/components/view/todos/todos.component.ts
+++ frontend/src/app/components/view/todos/todos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Todo } from '../../../todo';
+import { TodoService } from 'src/app/services/todo.service';
 
 
 @Component({
@@ -10,44 +11,54 @@ import { Todo } from '../../../todo';
 })
 export class TodosComponent implements OnInit {
 
-  readonly backend = 'http://localhost:3000/';
+  readonly backend = 'http://localhost:8080/api/';
 
   value: string;
   todos: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private _todos: TodoService) { }
 
   
   ngOnInit() {
     this.getTodos();
   }
 
+
+  // call GET from service
   getTodos(){
-    this.todos = this.http.get(this.backend + 'todos')
+    this._todos.GETtodos().subscribe(todos => {
+      this.todos = todos;
+      if(this.todos)
+        this.todos.sort((a,b) => a.id - b.id);
+    })
   }
 
-  deleteTodo(id){
-    let params = new HttpParams().set('id', id);
-    this.http.delete(this.backend + 'todos',{ params })
-    this.getTodos();
-  }
-
+  // call POST from service 
   addTodo(){
     if(!this.value){
       alert("GIVE TODO");
       return
     }
 
-    let todo = {
-      title: this.value
-    }
-    this.http.post( this.backend + '/todos', todo).subscribe(
-      res => console.log(res)
-    )
+    this._todos.POSTtodo(this.value).subscribe( res => {
+      this.getTodos();
+    })
+
+    this.value = '';
   }
 
+  // call DELETE from service
+  deleteTodo(id){
+    this._todos.DELETEtodo(id).subscribe(res => {
+      this.getTodos();
+    })
+  }
+
+  // call PATCH from service
   changeStatus(id){
-    let params = new HttpParams().set('id', id);
-    this.http.put(this.backend + '/todos', { params });
+    this._todos.PATCHtodo(id).subscribe(res => {
+      this.getTodos();
+    });
+    
   }
 }
